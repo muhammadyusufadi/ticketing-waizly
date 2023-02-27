3e714390ea4f36b318cbc14fe4f6722df6c35d68
muhammadyusufadi <muhammadyusufadiwibowo@gmail.com>
2023-02-27 16:58:57 +0700

    fix conflict edit_jabatan

--- application/views/back/jabatan/edit_jabatan.php
+++ application/views/back/jabatan/edit_jabatan.php
@@ -2,7 +2,15 @@
             Content body start
         ***********************************-->
 <div class="content-body">
-	<<<<<<< HEAD <div class="container-fluid">
+	<div class="container-fluid mt-3">
+		<div class="row">
+			<div class="col-lg-3 col-sm-6">
+				<h1>JABATAN</h1>
+			</div>
+		</div>
+		<!-- <div class="alert alert-success"> Selamat Datang <b> <?= $this->session->username; ?> </b> </div> -->
+	</div>
+	<div class="container-fluid mt-3">
 		<div class="row">
 			<div class="col-lg-6">
 				<div class="card">
@@ -15,39 +23,16 @@
 								<div class="form-group">
 									<label for="">Jabatan</label>
 									<input type="text" class="form-control input-default" placeholder="" name="jabatan">
-									=======
-									<div class="container-fluid mt-3">
-										<div class="row">
-											<div class="col-lg-3 col-sm-6">
-												<h1>JABATAN</h1>
-											</div>
-										</div>
-										<!-- <div class="alert alert-success"> Selamat Datang <b> <?= $this->session->username; ?> </b> </div> -->
-									</div>
-									<div class="container-fluid mt-3">
-										<div class="row">
-											<div class="col-lg-6">
-												<div class="card">
-													<div class="card-body">
-														<h4 class="card-title">Edit Jabatan</h4>
-														<?= $this->session->flashdata('message'); ?>
-														<?= validation_errors() ?>
-														<div class="table-responsive">
-															<form action="<?= base_url('jabatan/update_jabatan') ?>" method="POST">
-																<div class="form-group">
-																	<label for="">Jabatan</label>
-																	<input type="text" class="form-control input-default" placeholder="" name="jabatan">
-																	>>>>>>> d3eb5c355f8b7ccaa01962db2bcd888f2b33599b
 
-																</div>
-																<button type="submit" class="btn btn-primary btn-sm"> Save </button>
-																<button type="reset" class="btn btn-danger btn-sm"> Reset </button>
-															</form>
-														</div>
-													</div>
-												</div>
-											</div>
-											<!-- <div class="col-lg-6">
+								</div>
+								<button type="submit" class="btn btn-primary btn-sm"> Save </button>
+								<button type="reset" class="btn btn-danger btn-sm"> Reset </button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- <div class="col-lg-6">
 				<div class="card">
 					<div class="card-body">
 						<h4 class="card-title">Data Jabatan</h4>
@@ -85,11 +70,11 @@
 					</div>
 				</div>
 			</div> -->
-										</div>
-									</div>
-									<!-- #/ container -->
-								</div>
-								<!--**********************************
+		</div>
+	</div>
+	<!-- #/ container -->
+</div>
+<!--**********************************
 
             Content body end
 
